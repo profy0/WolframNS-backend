771f8474a55747c409d14c401a163a8eda5225eb
Nikita Yurchik <nikita-yurchik@mail.ru>
2024-02-20 20:26:43 +0300

    Initial commit

---
+++ src/test/java/com/NSiTeam/WolframNS/WolframNsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NSiTeam.WolframNS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WolframNsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
